86bf5098709dc50ed1b98b302d10ffb4a0e0f83e
Grigor Yosifov <grigor@forci.com>
2014-05-19 16:36:34 +0300

    Add created by

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,9 +1,10 @@
-
+/**
+ * Created by Grigor Yosifov on 5/15/2014.
+ */
 public class UserInterface {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
